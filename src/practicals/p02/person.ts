bf7d6b8777e037c2a83f23ba7a1810df4ab8d3a4
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-02-16 09:37:16 +0700

    commit

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,26 @@
-export class Person {}
+export class Person {
+    fisrtname: string
+    lastname: string
+    age: number = 0
+
+    constructor(firstname: string, lastname: string, age: number) {
+        this.fisrtname = firstname
+        this.lastname =lastname
+        this.age = age
+    }   
+
+    setAge(age:number) {
+        this.age + age
+    }
+    getAge() {
+        return `${this.age}`
+    }
+    getFullName() {
+        return `${this.fisrtname} ${this.lastname}`
+    }
+}
+
+const person = new Person("Pasa", "er",8) 
+console.log(person.getFullName());
+console.log(person.getAge);
+
